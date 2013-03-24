f6a9050f40d4a902dd81e82e04abb460d68bc13f
unknown <niklas@.(none)>
2013-03-24 17:59:52 +0100

    Allow rotating and draw squares.

--- chessboard.py
+++ chessboard.py
@@ -17,6 +17,7 @@ class Board(QWidget):
         self.darkSquareColor = QColor(100, 100, 255)
         self.borderColor = QColor(100, 100, 200)
         self.shadowWidth = 2
+        self.rotation = 20
 
     def paintEvent(self, event):
         painter = QPainter()
@@ -27,16 +28,31 @@ class Board(QWidget):
         backgroundBrush.setStyle(Qt.TexturePattern)
         painter.fillRect(QRect(QPoint(0, 0), self.size()), backgroundBrush)
 
+        # Do the rotation.
+        painter.translate(self.width() / 2, self.height() / 2)
+        painter.rotate(self.rotation)
+
         # Draw the border.
-        painter.translate(self.width() / 2.0, self.height() / 2.0)
-        frameSize = min(self.width(), self.height()) - self.margin
-        painter.fillRect(QRect(-frameSize / 2, -frameSize / 2, frameSize, frameSize), self.borderColor)
+        frameSize = min(self.width(), self.height()) - self.margin * 2
+        painter.translate(-frameSize / 2, -frameSize / 2)
+        painter.fillRect(QRect(0, 0, frameSize, frameSize), self.borderColor)
         painter.setPen(QPen(QBrush(self.borderColor.lighter()), self.shadowWidth))
-        painter.drawLine(-frameSize / 2, -frameSize / 2, -frameSize / 2, frameSize / 2)
-        painter.drawLine(-frameSize / 2, -frameSize / 2, frameSize / 2, -frameSize / 2)
+        painter.drawLine(0, 0, 0, frameSize)
+        painter.drawLine(0, 0, frameSize, 0)
         painter.setPen(QPen(QBrush(self.borderColor.darker()), self.shadowWidth))
-        painter.drawLine(frameSize / 2, -frameSize / 2, frameSize / 2, frameSize / 2)
-        painter.drawLine(-frameSize / 2, frameSize / 2, frameSize / 2, frameSize / 2)
+        painter.drawLine(frameSize, 0, frameSize, frameSize)
+        painter.drawLine(0, frameSize, frameSize, frameSize)
+
+        # Draw the squares.
+        painter.translate(self.margin, self.margin)
+        squareSize = (frameSize - 2 * self.padding) / 8.0
+        for x in range(0, 8):
+            for y in range(0, 8):
+                rect = QRect(x * squareSize, y * squareSize, squareSize, squareSize)
+                if (x - y) % 2 == 0:
+                     painter.fillRect(rect, QBrush(self.lightSquareColor))
+                else:
+                     painter.fillRect(rect, QBrush(self.darkSquareColor))
 
         painter.end()
 

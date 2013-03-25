7a51d0c108af33e6d1fc2dcaedb4f1bb58476150
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-03-25 18:48:10 +0100

    Make some more assertions about the games.

--- tests/test_chess.py
+++ tests/test_chess.py
@@ -8,3 +8,8 @@ class PgnDatabaseReaderTestCase(unittest.TestCase):
         reader = chess.PgnDatabaseReader("resources/games/kasparov-deep-blue-1997.pgn")
         reader.read_all()
         self.assertEqual(len(reader.games), 6)
+
+        second_game = reader.games[1]
+        self.assertEqual(second_game["White"], "Deep Blue (Computer)")
+        self.assertEqual(second_game["Result"], "1-0")
+        self.assertEqual(second_game["ECO"], "C93")
